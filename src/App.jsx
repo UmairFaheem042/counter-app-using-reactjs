1d63bc1b9c5de2bced10b978f450b0a0e25905d7
Umair <umair.freelancer042@gmail.com>
2024-02-17 20:27:45 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [value, setValue] = useState(0);
+  function handleDecrement() {
+    setValue((prev) => prev - 1);
+  }
+  function handleIncrement() {
+    setValue((prev) => prev + 1);
+  }
+  function handleReset() {
+    setValue(0);
+  }
+
+  return (
+    <div className="App">
+      <div className="counter-container">
+        <button onClick={handleIncrement}>+</button>
+        <h1 className="value">{value}</h1>
+        <button onClick={handleDecrement}>-</button>
+      </div>
+      <button className="reset" onClick={handleReset}>
+        RESET
+      </button>
+    </div>
+  );
+};
+
+export default App;
